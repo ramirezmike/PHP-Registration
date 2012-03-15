8a1b7888add9a858b1025b6fd7787614171f2e64
Michael Ramirez <ramirezmike2@gmail.com>
2012-03-15 08:32:18 -0400

    Cleaned up main.php

--- main.php
+++ main.php
@@ -1,104 +1,114 @@
+<?php 
+	session_start(); 
+	session_regenerate_id();
+?>
 <html>
-<body>
+	<body>
 
-<a href="index.php">Index</a></br>
+		<a href="index.php">Index</a></br>
 
 <?php
 
-function myHash($input) {
-	return hash('sha512', $input);
-}
-
-function unhash($input) {
-	return str_rot13(base64_decode($input));
-}
-
-$connection = mysql_connect("localhost","root");
-if (!$connection)
-	{
-	die( mysql_error());
-	}
-
-mysql_select_db("db_users", $connection);
-
-$adminRow=mysql_fetch_array(mysql_query("SELECT * FROM logininfo WHERE userID=1"));
-if (!$_POST[deleteuser] == 1) {
-mysql_query("DELETE FROM logininfo WHERE userID='$_POST[deleteuser]'");
-}
-
-$loginRow=mysql_query("SELECT * FROM logininfo WHERE loginname='$_POST[loginname]'");
-$nameForCheck=mysql_fetch_array($loginRow);
-if ($nameForCheck['password'] == myHash($_POST['password'])) {
-
-$result = mysql_query('SELECT * FROM logininfo',$connection); 
-$rownumbers = mysql_num_rows($result);
-if ($_POST['loginname'] == '' or $_POST['password'] == '')
+	function myHash($input) 
 {
-	echo "Error: Blank fields";
+		return hash('sha512', $input);
 }
-else if ($_POST['loginname'] == $adminRow['loginname'])
+
+	function unhash($input) 
 {
-
-echo '<div id="table" style="float:left;">';
-echo "<table border='1'>
-<tr>
-<th>UserID</th>
-<th>LoginName</th>
-<th>Password</th>
-</tr>";
-while ($row = mysql_fetch_array($result))
-{
-echo "<tr>";
-echo "<td>" . $row['userID'] . "</td>";
-echo "<td>" . $row['loginname'] . "</td>";	
-echo "<td>" . $row['password'] . "</td>";	
-echo "</tr>";
-}
-echo "</table>";
-echo "</div>";
-echo "<br />";
-echo "Logged in as Admin";
-echo "<form action='main.php' method='post'>";
-echo "Delete by userID <br />";
-echo "<input type='text' name='deleteuser' /><br />";
-echo "<input type='submit' value='Delete' />";
-echo "</form>";
-}
-else if ($rownumbers )
-	{
-	echo "Login found<br /";
-	echo "$rownumbers Rows\n";
-	echo '<div id="table" style="float:left;">';
-	echo "<table border='1'>
-	<tr>
-	<th>UserID</th>
-	<th>LoginName</th>
-	<th>Password</th>
-	</tr>";
-	while ($row = mysql_fetch_array($result))
-	{
-	echo "<tr>";
-	echo "<td>" . $row['userID'] . "</td>";
-	echo "<td>" . $row['loginname'] . "</td>";	
-	if ($row['password'] == myHash($_POST['password'])) {
-		echo "<td>" . $row['password'] . "</td>";	
-	}
-	echo "</tr>";
-	}
-	echo "</table>";
-	echo "</div>";
-	}
-else
-	{
-	echo "Login not found";
-	}
-
+		return str_rot13(base64_decode($input));
 }
 
-else {
-	echo "Login Unsuccesful";
-}
-mysql_close($connection);
+	$connection = mysql_connect("localhost","root");
+		if (!$connection)
+		{
+				die( mysql_error());
+		}
+	mysql_select_db("db_users", $connection);
+
+	$adminRow=mysql_fetch_array(mysql_query("SELECT * FROM logininfo WHERE userID=1"));
+	$loginRow=mysql_query("SELECT * FROM logininfo WHERE loginname='$_SESSION[loginname]'");
+	$nameForCheck=mysql_fetch_array($loginRow);
+
+	if (!$_POST[deleteuser] == 1) 
+	{
+		mysql_query("DELETE FROM logininfo WHERE userID='$_POST[deleteuser]'");
+	}
+
+	if ($nameForCheck['password'] == myHash($_SESSION['password'])) 
+	{
+		$result = mysql_query('SELECT * FROM logininfo',$connection); 
+		$rownumbers = mysql_num_rows($result);
+		if ($_SESSION['loginname'] == '' or $_SESSION['password'] == '')
+		{
+			echo "Error: Blank fields";
+		}
+		else if ($_SESSION['loginname'] == $adminRow['loginname'])
+		{
+
+			echo '<div id="table" style="float:left;">';
+			echo "<table border='1'>
+				<tr>
+				<th>UserID</th>
+				<th>LoginName</th>
+				<th>Password</th>
+				</tr>";
+			while ($row = mysql_fetch_array($result))
+			{
+				echo "<tr>";
+				echo "<td>" . $row['userID'] . "</td>";
+				echo "<td>" . $row['loginname'] . "</td>";	
+				echo "<td>" . $row['password'] . "</td>";	
+				echo "</tr>";
+			}
+			echo "</table>";
+			echo "</div>";
+			echo "<br />";
+			echo "Logged in as Admin";
+			echo "<form action='main.php' method='post'>";
+			echo "Delete by userID <br />";
+			echo "<input type='text' name='deleteuser' /><br />";
+			echo "<input type='submit' value='Delete' />";
+			echo "</form>";
+		}
+		else if ($rownumbers )
+		{
+			echo "Login found<br /";
+			echo "$rownumbers Rows\n";
+			echo '<div id="table" style="float:left;">';
+			echo "<table border='1'>
+				<tr>
+				<th>UserID</th>
+				<th>LoginName</th>
+				<th>Password</th>
+				</tr>";
+			while ($row = mysql_fetch_array($result))
+			{
+				echo "<tr>";
+				echo "<td>" . $row['userID'] . "</td>";
+				echo "<td>" . $row['loginname'] . "</td>";	
+				if ($row['password'] == myHash($_SESSION['password'])) 
+				{
+					echo "<td>" . $row['password'] . "</td>";	
+				}
+				echo "</tr>";
+			}
+			echo "</table>";
+			echo "</div>";
+		}
+		else
+		{
+			echo "Login not found";
+		}
+
+	}
+
+	else 
+	{
+		echo "Login Unsuccesful";
+	}
+
+	mysql_close($connection);
 ?>
-</body>
+	</body>
 </html>
