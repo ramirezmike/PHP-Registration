4682447bfdce0e6bf9adc3225526c177371bca2c
Michael Ramirez <ramirezmike2@gmail.com>
2012-03-15 08:36:54 -0400

    Cleaned up register.php

--- register.php
+++ register.php
@@ -1,69 +1,68 @@
 <html>
-<body>
+	<body>
 
-<a href="index.php">Index</a></br>
+		<a href="index.php">Index</a></br>
 
 <?php
-function myHash($input)
-{
-	return hash('sha512', $input);
-}
-
-function dehash($input)
-{
-	return str_rot13(base64_decode($input));
-}
-
-$connection = mysql_connect("localhost","root");
-if (!$connection)
+	function myHash($input)
 	{
-	die( mysql_error());
-	}
-if (!mysql_select_db("db_users", $connection))
-	{
-	mysql_query("CREATE DATABASE db_users", $connection);
-	echo "Database created";
-	}
-else
-	{
-	echo  mysql_error();
+		return hash('sha512', $input);
 	}
 
-mysql_select_db("db_users", $connection);
-$tables=mysql_query("SHOW TABLES IN db_users");
-echo($tablerows[pass]);
-while ($rows=mysql_fetch_array($tables)){
-	echo $row['loginname'];
-}
+	$connection = mysql_connect("localhost","root");
+	if (!$connection)
+	{
+		die( mysql_error());
+	}
+	if (!mysql_select_db("db_users", $connection))
+	{
+		mysql_query("CREATE DATABASE db_users", $connection);
+		echo "Database created";
+	}
+	else
+	{
+		echo  mysql_error();
+	}
+
+	mysql_select_db("db_users", $connection);
+	$tables=mysql_query("SHOW TABLES IN db_users");
+	echo($tablerows[pass]);
+	while ($rows=mysql_fetch_array($tables))
+	{
+		echo $row['loginname'];
+	}
+
 	$sql = "CREATE TABLE IF NOT EXISTS logininfo
-(
-userID INT(3) NOT NULL AUTO_INCREMENT,
-loginname varchar(15) NOT NULL,
-password varchar(128) NOT NULL,
-PRIMARY KEY (userID),
-UNIQUE KEY (loginname)
-)";
+		(
+		userID INT(3) NOT NULL AUTO_INCREMENT,
+		loginname varchar(15) NOT NULL,
+		password varchar(128) NOT NULL,
+		PRIMARY KEY (userID),
+		UNIQUE KEY (loginname)
+		)";
 
-mysql_query($sql,$connection);
+	mysql_query($sql,$connection);
 
-if ($_POST[registername] == '' || $_POST[registerpassword] == '')
-{
-	echo "Registration Unsuccessful<br />";
-}
-else
-{
-	$password = myHash($_POST[registerpassword]);
-	$sql="INSERT INTO logininfo (loginname, password) VALUES ('$_POST[registername]','$password')";
-		
-	if (!mysql_query($sql,$connection)) {
-		die('Error: ' . mysql_error());
+	if ($_POST[registername] == '' || $_POST[registerpassword] == '')
+	{
+		echo "Registration Unsuccessful<br />";
 	}
+	else
+	{
+		$password = myHash($_POST[registerpassword]);
+		$sql="INSERT INTO logininfo (loginname, password) VALUES ('$_POST[registername]','$password')";
+			
+		if (!mysql_query($sql,$connection)) 
+		{
+			die('Error: ' . mysql_error());
+		}
 
-	else {
-		echo "Registration Successful<br />";
+		else 
+		{
+			echo "Registration Successful<br />";
+		}
 	}
-}
-mysql_close($connection);
+	mysql_close($connection);
 ?>
-</body>
+	</body>
 </html>
