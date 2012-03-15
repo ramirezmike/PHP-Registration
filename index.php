9b6050bba85e608040dc7656302437066d73ca5a
Michael Ramirez <ramirezmike2@gmail.com>
2012-03-14 22:21:18 -0400

    Changed password text field to type password

--- index.php
+++ index.php
@@ -4,13 +4,13 @@
 <form action="main.php" method="post">
 Login<br />
 Name:<br /> <input type = "text" name="loginname" /><br />
-Password:<br /> <input type = "text" name="password" /><br />
+Password:<br /> <input type = "password" name="password" /><br />
 <input type="submit" value="Enter">
 </form>
 <form action="register.php" method="post">
 Register<br />
 Name:<br /> <input type = "text" name="registername" /><br />
-Password:<br /> <input type = "text" name="registerpassword" /><br />
+Password:<br /> <input type = "password" name="registerpassword" /><br />
 <input type="submit" value="Register" />
 </form>
 
